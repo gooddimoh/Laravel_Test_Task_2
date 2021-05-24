a445357f75b93466065b276b66e724e00903c669
Dmitriy <gooddimoh@gmail.com>
2021-05-24 19:21:03 +0300

    Commit Test Task

---
+++ app/Http/Controllers/Clients.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Clients extends Controller
+{
+    public function index(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function delete(){
+
+    }
+}
---
+++ database/seeders/CLIENTS.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CLIENTS extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
